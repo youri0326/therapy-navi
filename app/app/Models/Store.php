757b0a8f0fdfe93911f195544c1a514ecf67e283
ysato <ysprimacc4@gmail.com>
2023-11-15 22:51:42 +0900

    ゲッターとセッターの追加

--- app/app/Models/Store.php
+++ app/app/Models/Store.php
@@ -40,4 +40,114 @@ class Store
         //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
         $this->storeid = $storeid;
     }
+
+    //accountidゲッター
+    public function getAccountId(){
+        return $this->accountid;
+    }
+
+    //accountidセッター
+    public function setAccountId($accountid){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->storeid = $accountid;
+    }
+
+    //storenameゲッター
+    public function getStoreName(){
+        return $this->storename;
+    }
+
+    //storenameセッター
+    public function setStoreName($storename){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->storename = $storename;
+    }
+
+    //adressゲッター
+    public function getAdress(){
+        return $this->adress;
+    }
+
+    //addressセッター
+    public function setAdress($adress){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->address = $address;
+    }
+
+    //accountidゲッター
+    public function getBudget(){
+        return $this->budget;
+    }
+
+    //accountidセッター
+    public function setAccountId($budget){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->budget = $budget;
+    }
+
+    //commentゲッター
+    public function getComment(){
+        return $this->comment;
+    }
+
+    //commentセッター
+    public function setComment($comment){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->storeid = $comment;
+    }
+
+    //paymentゲッター
+    public function getPayment(){
+        return $this->accountid;
+    }
+
+    //paymentセッター
+    public function setPayment($payment){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->payment = $payment;
+    }
+
+    //storemenuListゲッター
+    public function getStoreMenuList(){
+        return $this->getStoreMenuList;
+    }
+
+    //storemenuListセッター
+    public function setStoreMenuList($storemenuList){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->storemenuList = $storemenuList;
+    }
+
+    //storephotoListゲッター
+    public function getStorePhotoList(){
+        return $this->storephotoList;
+    }
+
+    //storephotoListセッター
+    public function setStorePhotoList($storephotoList){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->storephotoList = $storephotoList;
+    }
+
+    //stationListゲッター
+    public function getStationList(){
+        return $this->stationList;
+    }
+
+    //stationListセッター
+    public function setStationList($stationList){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->stationList = $stationList;
+    }
+
+    //staffListゲッター
+    public function getStaffList(){
+        return $this->staffList;
+    }
+
+    //staffListセッター
+    public function setStaffList($staffList){
+        //更新したい値を引数にとり、$this->priceに代入し、privateプロパティを更新
+        $this->stationList = $staffList;
+    }
 }
