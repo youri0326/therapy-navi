b3c73eed8d6c4cb78beb7b3c48726259fcfb0656
ysato <ysprimacc4@gmail.com>
2023-11-23 22:53:38 +0900

    StoreStaffListController.phpの作成

---
+++ app/app/Models/staffinfo.php
@@ -0,0 +1,30 @@
+<?php
+
+// accountinfo.phpからコピーして作成
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class accountinfo extends Model
+{
+    use HasFactory;
+    
+    // このモデルの対象のテーブル
+    protected $table = 'accountinfo';
+    
+    // プライマリキーを指定
+    protected $primarykey = 'accountid';
+    
+    // プライマリーキーがオートインクリメントではないため設定をオフ
+    public $incrementing = false;
+    
+    // データの作成日時、更新日時がデフォルトで自動更新されるので、オフ
+    public $timestamps = false;
+
+    public function storeinfo()
+    {
+        return $this->hasOne('App\Models\storeinfo', 'accountid', 'accountid');
+    }
+}
