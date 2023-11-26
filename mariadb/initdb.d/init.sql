50d451e0df7231c2e7c39a3b431c2480331a948c
youri0326 <youriyoshiike@gmail.com>
2023-11-26 14:43:57 +0900

    データベース登録データの更新

--- mariadb/initdb.d/init.sql
+++ mariadb/initdb.d/init.sql
@@ -1,15 +1,5 @@
 
 USE therapyNavi-db;
-CREATE TABLE test(
-    id INT NOT NULL,
-    name VARCHAR(15),
-    price INT,
-    PRIMARY KEY (id)
-);
-
-INSERT INTO test(id,name,price) VALUES('0','田中','4000');
-INSERT INTO test(id,name,price) VALUES('1','佐藤','3000');
-INSERT INTO test(id,name,price) VALUES('2','鈴木','2000');
 
 CREATE TABLE bookinfo (
     isbn VARCHAR(20) PRIMARY KEY,
@@ -24,11 +14,12 @@ INSERT INTO bookinfo VALUES('0004','perl',1004);
 INSERT INTO bookinfo VALUES('0005','database',1005);
 
 CREATE TABLE accountinfo ( 
-    accountid VARCHAR(20) PRIMARY KEY,
-    password VARCHAR(20),
+    accountid INTEGER AUTO_INCREMENT PRIMARY KEY,
+    loginid VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(20) NOT NULL UNIQUE,
     email VARCHAR(100),
     phone VARCHAR(100),
     authority INTEGER
 )ENGINE = InnoDB,DEFAULT CHARSET=utf8;
 
-INSERT INTO accountinfo (accountid,password,email,phone,authority) VALUES('0001','test','youriyoshiike@gmail.com','000-0000-0000',0);
+INSERT INTO `accountinfo` (`accountid`, `loginid`,`password`, `email`, `phone`, `authority`) VALUES (0,'0001', 'test', ''youriyoshiike@gmail.com', '000-0000-0000', 0);
