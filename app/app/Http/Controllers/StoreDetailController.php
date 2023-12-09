73dfc245a62a0adbb6a395b1431778bc35f707dc
ysato <ysprimacc4@gmail.com>
2023-12-09 22:16:50 +0900

    idを指定可能に変更

--- app/app/Http/Controllers/StoreDetailController.php
+++ app/app/Http/Controllers/StoreDetailController.php
@@ -8,10 +8,10 @@ use App\Models\storeinfo;
 // ユーザーが各店舗の詳細をクリック後表示
 class StoreDetailController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         
         // storeidを取得仮で3
-        $storeid = 3;
+        $storeid = $request->query('storeid');
         // 上記のstoreidの時の店舗情報をstoreinfoのテーブルから該当行を持ってくる
         // モデル名：where('列名', '=', 検索値)->get();
         $store = storeinfo::where('storeid', '=', $storeid)->get();// 変数に代入
