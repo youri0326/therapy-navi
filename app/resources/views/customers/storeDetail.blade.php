e13941e0a057939b3c4338e375bfde5bb0d86785
ysato <ysprimacc4@gmail.com>
2023-12-10 14:26:54 +0900

    店舗メニューへのリンクの作成

--- app/resources/views/customers/storeDetail.blade.php
+++ app/resources/views/customers/storeDetail.blade.php
@@ -14,33 +14,34 @@
 		<h1 align="center" style="margin-top: 21px;">店舗詳細画面</h1>
 		<hr align="center" size="5" color="BLUE" width="950"></hr>
 			<!-- メニュー -->
-			@foreach($store as $store)
-				<a href ="{{asset('/customers/storeDetail')}}?storeid={{$store->storeid}}">メニュー</a>
-			@endforeach
+
 			<!-- 店舗の詳細情報 -->
 			<div id="main" class="container">
 				<table class="input-table">
 					<tbody>
-					@foreach($store as $store)
-					<tr>
-               			<th>店舗ID</th><td>{{$store->storeid}}<td>
-					</tr>
-               		<tr>
-						<th>店舗名</th><td>{{$store->storename}}</td>
-					</tr>
-					<tr>
-						<th>住所</th><td>{{$store->address}}</td>
-					</tr>
-					<tr>
-						<th>予算</th><td>{{$store->budget}}</td>
-					</tr>
-					<tr>
-						<th>備考欄</th><td>{{$store->comment}}</td>
-					</tr>
-					<tr>
-						<th>支払方法</th><td>{{$store->payment}}</td>
-					</tr>
-					@endforeach
+						@foreach($store as $store)
+						<tr>
+							<a href ="{{asset('/customers/storeMenu')}}?storeid={{$store->storeid}}">メニュー</a>
+						</tr>
+						<tr>
+							<th>店舗ID</th><td>{{$store->storeid}}<td>
+						</tr>
+						<tr>
+							<th>店舗名</th><td>{{$store->storename}}</td>
+						</tr>
+						<tr>
+							<th>住所</th><td>{{$store->address}}</td>
+						</tr>
+						<tr>
+							<th>予算</th><td>{{$store->budget}}</td>
+						</tr>
+						<tr>
+							<th>備考欄</th><td>{{$store->comment}}</td>
+						</tr>
+						<tr>
+							<th>支払方法</th><td>{{$store->payment}}</td>
+						</tr>
+						@endforeach
 					</tbody>
 				</table>
 			</div>
