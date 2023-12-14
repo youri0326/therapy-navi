87dad7d3b63a186ac474db435c59118ab64435f7
ysato <ysprimacc4@gmail.com>
2023-12-14 21:18:30 +0900

    モデルの修正

--- app/app/Http/Controllers/StoreInsertController.php
+++ app/app/Http/Controllers/StoreInsertController.php
@@ -8,5 +8,14 @@ use App\Models\storeinfo;
 class StoreDetailController extends Controller
 {
     public function index() {
+        // storeinfoテーブルに登録
+
     }
+
+    // 登録画面
+    public function create(Request $request) {
+        return view('');
+    }
+    // 登録処理
+    public function store()
 }
