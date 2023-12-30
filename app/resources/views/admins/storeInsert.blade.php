e2da93ee67d0e09b0d0217fb4a08eb76daf9220d
ysato <ysprimacc4@gmail.com>
2023-12-31 00:21:22 +0900

    ブレイドの修正

--- app/resources/views/admins/storeInsert.blade.php
+++ app/resources/views/admins/storeInsert.blade.php
@@ -9,7 +9,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta http-equiv="Content-Style-Type" content="text/css" />
-		<title>List</title>
+		<title>店舗登録</title>
 	</head>
 	<body>
 		<h1 align="center" style="margin-top: 21px;">店舗登録画面</h1>
@@ -21,11 +21,28 @@
 
             <form action="{{ route('store.confirm') }}" method="post">
                 @csrf
-                <input type="text" name="storename" placeholder="店舗名">
-                <input type="text" name="address" placeholder="住所">
-                <input type="text" name="budget" placeholder="予算">
-                <input type="text" name="comment" placeholder="コメント">
-                <input type="text" name="payment" placeholder="支払い方法">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>店舗名*</th>
+                            <td><input type="text" name="storename" placeholder="店舗名"></td>
+                        </tr>
+                        <tr>
+                            <th>住所*</th>
+                            <td><input type="text" name="address" placeholder="住所"></td>
+                        </tr>
+                        <tr>
+                            <th>予算</th>
+                            <td><input type="text" name="budget" placeholder="予算"></td>
+                        </tr>
+                            <th>コメント</th>
+                            <td><input type="text" name="comment" placeholder="コメント"></td>
+                        <tr>
+                            <th>支払方法</th>
+                            <td><input type="text" name="payment" placeholder="支払い方法"></td>
+                        </tr>
+                    </tbody>
+                </table>
                 <!-- 他のフォームフィールドもここに追加 -->
                 <button type="submit">確認</button>
             </form>
