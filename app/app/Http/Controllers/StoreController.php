e59d3dc12aec7b995d455269dc618615e1dbcbf7
youri0326 <youriyoshiike@gmail.com>
2023-12-31 14:24:05 +0900

    コントローラの作成正

---
+++ app/app/Http/Controllers/StoreController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\storeinfo;
+
+// ユーザーが各店舗の詳細をクリック後表示
+class StoreController extends Controller
+{
+    public function detailByAdmin(Request $request) {
+        
+        // URLからstoreidを取得
+        $storeid = $request->query('storeid');
+        // 上記のstoreidの時の店舗情報をstoreinfoのテーブルから該当行を持ってくる
+        // モデル名：where('列名', '=', 検索値)->get();
+        $store = storeinfo::where('storeid', '=', $storeid)->get();// 変数に代入
+        
+        // 
+        return view('customers/storeDetail',[ // ひな形を指定
+            'store' => $store
+        ]);
+    }
+    
+}
