e439e9f8afb7c143305c9e63a00a11124fd3d670
youri0326 <youriyoshiike@gmail.com>
2024-01-07 19:59:31 +0900

    ホームに都道府県の追加

--- app/app/Http/Controllers/CustomerHomeController.php
+++ app/app/Http/Controllers/CustomerHomeController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\regioninfo;
+use App\Models\prefectureinfo;
 
 class CustomerHomeController extends Controller
 {
     public function index() {
-        
+        $regions = regioninfo::all();
+
+        $prefectures = [];
+        foreach ($regions as $region) {
+            $prefectures[$region->regionid] = prefectureinfo::where('regionid', $region->regionid)->get();
+
+        }
         // 顧客ログイン画面へ書籍情報と一緒に遷移
-        return view('customers/customerHome');
+        return view('customers/customerHome',compact('regions', 'prefectures'));
     }
     
 }
