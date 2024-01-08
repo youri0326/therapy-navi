aa24b6382e868ab605704b512057e9901ed9d086
youri0326 <youriyoshiike@gmail.com>
2024-01-08 15:29:26 +0900

    検索ページに画像とメニューの表示の修正

--- app/app/Http/Controllers/StoreController.php
+++ app/app/Http/Controllers/StoreController.php
@@ -6,10 +6,34 @@ use Illuminate\Http\Request;
 use App\Models\storeinfo;
 use App\Models\stationinfo;
 use App\Models\storephotoinfo;
+use App\Models\storemenuinfo;
 
 // ユーザーが各店舗の詳細をクリック後表示
 class StoreController extends Controller
 {
+    public function search(Request $request) {
+        
+        //入力フォームの情報の受け取り
+        $address = $request->query('address');
+        $station = $request->query('station');
+        $storename = $request->query("storename");
+        $comment = $request->query("comment");
+
+        //storeinfoモデルのインスタンス化
+        $objStore = new storeinfo();
+        
+        // 【検索データの取得】
+        // 入力フォームの受け取り情報を検索値に、storeinfoテーブルから次のテーブルを結合(joinする形で)該当する行を取得        
+        $storeList = $objStore->searchStore($address,$station, $storename,$comment);
+
+        // 検索画面へ店舗情報と一緒に遷移
+        return view('customers/storeSearch',[
+            'storeList' => $storeList,
+            'address' => $address,
+            'station' => $station,
+        ]);
+        
+    }
     public function detailByAdmin(Request $request) {
         
         //後々はログイン情報から取得できるようにする
