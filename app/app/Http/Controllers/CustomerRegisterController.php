e2cd76c75d2617c84a07e0ecb28fabee12cc6324
youri0326 <youriyoshiike@gmail.com>
2024-01-14 00:16:51 +0900

    顧客登録機能の追加

--- app/app/Http/Controllers/CustomerRegisterController.php
+++ app/app/Http/Controllers/CustomerRegisterController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\customerinfo;
 use App\Models\user;
 use Exception;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerRegisterController extends Controller
 {
@@ -34,33 +35,32 @@ class CustomerRegisterController extends Controller
       $customer->birthday = $request->birthday;
       $customer->address = $request->address;
       // バリデーション
-      // $request->validate([
-      //     'name' => 'required|max:255',
-      //     'furigana' => 'required|max:255',
-      //     'birthday' => 'required|date',
-      //     'address' => 'required|max:255',
-      //     'email' => 'required|email',
-      //     'phone' => 'required|min:9|max:15',
-      //     'loginid' => 'required|max:255',
-      //     'password' => 'required|min:4|max:255',
-      // ]);
+      $rules = [
+          'name' => 'required|max:255',
+          'furigana' => 'required|max:255|regex:/^[ぁ-んァ-ンー]+$/u',
+          'birthday' => 'required|date',
+          'address' => 'required|max:255',
+          'email' => 'required|email',
+          'phone' => 'required|min:9|max:15',
+          'loginid' => 'required|max:255',
+          'password' => 'required|min:4|max:255',
+      ];
+      $validator = Validator::make($request->all(), $rules);
 
-      // バリデーションに失敗した場合
-      // if ($request->hasErrors()) {
-      //     // 登録フォームに戻る
-      //     $error = 'jjjエラーが発生しました、再入力してください。';
-      // }
+      //バリデーションに失敗した場合
+      if ($request->$validator->fails()) {
+          // 登録フォームに戻る
+          $error = 'エラーが発生しました、再入力してください。';
+      }
 
     } catch (Exception $e) {
         // エラー処理
-        $error = 'エラーが発生しました、再入力してください。kkk';
+        $error = 'エラーが発生しました、再入力してください。';
     }finally{
         if($error===""){
             // 確認画面を表示
             return view('customers/registration/confirmation', compact('user', 'customer'));
         }else{
-            $link['href'] = 'home';
-            $link['text'] = 'ホーム画面に戻る';
             return redirect()->route('customers.registration.showForm')->with(compact('error'));;
         }
     }
@@ -68,18 +68,23 @@ class CustomerRegisterController extends Controller
   
   public function insert(Request $request)
   {
-    // バリデーション
-    // $request->validate([
-    //   'name' => 'required|max:255',
-    //   'furigana' => 'required|max:255|regex:/^[ぁ-んァ-ンー]+$/u',
-    //   'birthday' => 'required|date',
-    //   'address' => 'required|max:255',
-    //   'email' => 'required|email',
-    //   'phone' => 'required|min:10|max:11|numeric',
-    //   'loginid' => 'required|max:255|unique:userinfo',
-    //   'password' => 'required|min:8|max:255',
-    // ]);
+    $rules = [
+      'name' => 'required|max:255',
+      'furigana' => 'required|max:255|regex:/^[ぁ-んァ-ンー]+$/u',
+      'birthday' => 'required|date',
+      'address' => 'required|max:255',
+      'email' => 'required|email',
+      'phone' => 'required|min:9|max:15',
+      'loginid' => 'required|max:255',
+      'password' => 'required|min:4|max:255',
+    ];
+    $validator = Validator::make($request->all(), $rules);
 
+    //バリデーションに失敗した場合
+    if ($request->$validator->fails()) {
+        // 登録フォームに戻る
+        $error = 'エラーが発生しました、再入力してください。';
+    }
 
     // ユーザー情報の作成
     $user= new user();
