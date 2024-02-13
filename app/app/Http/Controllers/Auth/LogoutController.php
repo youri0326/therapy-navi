4909297ee9bfe980baa85a3310a4ecbd2be0f205
ysato <ysprimacc4@gmail.com>
2024-02-13 22:17:43 +0900

    微修正

--- app/app/Http/Controllers/Auth/LogoutController.php
+++ app/app/Http/Controllers/Auth/LogoutController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 
 class LogoutController extends Controller
 {
